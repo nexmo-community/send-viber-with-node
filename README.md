bfa8ba4e9911d66d8823fedcca57f79234fb805d
Garann Means <garann.means@vonage.com>
2020-04-14 10:13:52 +0100

    Added .env and days of the week

---
+++ README.md
@@ -0,0 +1,16 @@
+# Send a Viber Message with Node
+
+This file sends a test Viber message using Node and the Vonage Developer Messaging sandbox. To use it, first go to your [Vonage Developer](https://dashboard.nexmo.com/) dashboard and configure your [Viber sandbox](https://dashboard.nexmo.com/messages/sandbox).
+
+Clone the repository to a new directory on your machine and:
+
+1. Run `npm install` to add [axois](https://github.com/axios/axios) for making requests and support for a `.env` file
+2. Create a new `.env` file and populate it with your credentials:
+   ```text
+   USERNAME="12ab3456"
+   PASSWORD="123AbcdefghIJklM"
+   FROM_ID="12345"
+   TO_NUM="441234567890"
+   ```
+   Your username and password are your Vonage API key and secret. You can copy the from ID from the cURL command at the bottom of the sandbox page, and the to number is the device number you've whitelisted.
+3. Run `node app.js` to send a test message
--- app.js
+++ app.js
@@ -1,12 +1,29 @@
+require('dotenv').config();
 const axios = require('axios');
 
+var user = process.env.USERNAME;
+var password = process.env.PASSWORD;
+var from_id = process.env.FROM_ID;
+var to_num = process.env.TO_NUM;
+
+const weekdays = [
+  'Sunday',
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday'
+];
+var today = weekdays[new Date().getDay()];
+
 axios.post('https://messages-sandbox.nexmo.com/v0.1/messages',{
-    "from": { "type": "viber_service_msg", "id": "" },
-    "to": { "type": "viber_service_msg", "number": "" },
+    "from": { "type": "viber_service_msg", "id": from_id },
+    "to": { "type": "viber_service_msg", "number": to_num },
     "message": {
       "content": {
         "type": "text",
-        "text": "This is a Viber Service Message sent from the Messages API"
+        "text": "Hello from Vonage! Happy " + today
       }
     }
   },{
@@ -14,8 +31,8 @@ axios.post('https://messages-sandbox.nexmo.com/v0.1/messages',{
       'Content-Type': 'application/json'
     },
     auth: {
-      username: '',
-      password: ''
+      username: user,
+      password: password
     }
   })
   .then(function (response) {
